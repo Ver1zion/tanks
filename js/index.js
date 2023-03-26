522df2e16ec284044dcce67e5e774b6cdcf4d94b
Igor <smmdirektolog@yandex.ru>
2023-03-26 06:13:44 +0300

    +

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let socket = new WebSocket("ws://84.252.130.243:8080");
+let socket = new WebSocket("ws://51.250.69.80:8080");
 
 socket.onopen = () => {
   alert("Соединение установлено");
@@ -8,7 +8,7 @@ socket.onclose = (event) => {
   alert(`Соединение разорвано, код: ${event.code}`);
 };
 
-const tank = document.querySelector(".tank");
+const fire = document.querySelector(".fire");
 const wall1 = document.querySelector(".wall-1");
 const wall2 = document.querySelector(".wall-2");
 const wall3 = document.querySelector(".wall-3");
@@ -23,32 +23,42 @@ document.addEventListener("keyup", (event) => {
     socket.send(`{"Move":"Up"}`);
   } else if (event.key === "ArrowDown") {
     socket.send(`{"Move":"Down"}`);
+  } else if (event.code === "Space") {
+    if (fire.classList.contains("addFire")) return;
+    // socket.send(`{"Move":"Fire"}`);
+    fire.classList.add("addFire");
+    fire.style.backgroundImage =
+      "url(../img/fire.gif?random=" + Math.random() + ")";
+    setTimeout(() => {
+      fire.style.backgroundImage = "";
+      fire.classList.remove("addFire");
+    }, 800);
   }
-
-  socket.onmessage = (event) => {
-    const dataParsed = JSON.parse(event.data);
-    console.log(dataParsed);
-    const x = dataParsed.X;
-    const y = dataParsed.Y;
-    const rotate = dataParsed.R;
-    tank.style.transform = rotate + "deg";
-    tank.style.left = x + "px";
-    tank.style.top = y + "px";
-  };
 });
 
-const players = {
-  Players: [
-    { Id: 0, X: 20, Y: 40 },
-    { Id: 1, X: 20, Y: 40 },
-  ],
+socket.onmessage = (event) => {
+  const dataParsed = JSON.parse(event.data);
+  console.log(dataParsed);
+  dataParsed.Players.forEach((player) => {
+    if (!document.querySelector(`.tank${player.Id}`)) {
+      const newTank = document.createElement("div");
+      newTank.classList.add(`tank${player.Id}`);
+      newTank.style.position = "absolute";
+      newTank.style.top = `${player.X}px`;
+      newTank.style.left = `${player.Y}px`;
+      newTank.style.width = "100px";
+      newTank.style.height = "124px";
+      newTank.style.backgroundImage = "url(../img/Tank.png)";
+      newTank.style.backgroundRepeat = "no-repeat";
+      newTank.style.backgroundSize = "cover";
+      newTank.style.transition = "left 0.3s linear, top 0.3s linear";
+      newTank.style.transform = `rotate(${player.R}deg)`;
+      document.body.prepend(newTank);
+    } else {
+      const tank = document.querySelector(`.tank${player.Id}`);
+      tank.style.transform = `rotate(${player.R}deg)`;
+      tank.style.left = `${player.X}px`;
+      tank.style.top = `${player.Y}px`;
+    }
+  });
 };
-
-if (players.Players.length === 2) {
-  const tank2 = tank.cloneNode();
-  tank2.classList.remove("tank");
-  tank2.classList.add("tank-2");
-  document.body.append(tank2);
-}
-
-socket.onmessage = (event) => {};
