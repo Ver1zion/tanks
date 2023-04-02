df117e79c459637de290cb6c15bc47f021952bd8
Igor <smmdirektolog@yandex.ru>
2023-04-03 01:02:49 +0300

    +

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let socket = new WebSocket("ws://51.250.69.80:8080");
+let socket = new WebSocket("ws://localhost:8080");
 
 socket.onopen = () => {
   alert("Соединение установлено");
@@ -8,13 +8,13 @@ socket.onclose = (event) => {
   alert(`Соединение разорвано, код: ${event.code}`);
 };
 
-const fire = document.querySelector(".fire");
+const moveArray = ["ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"];
 const wall1 = document.querySelector(".wall-1");
 const wall2 = document.querySelector(".wall-2");
 const wall3 = document.querySelector(".wall-3");
 const wall4 = document.querySelector(".wall-4");
 
-document.addEventListener("keyup", (event) => {
+document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowLeft") {
     socket.send(`{"Move":"Left"}`);
   } else if (event.key === "ArrowRight") {
@@ -24,15 +24,13 @@ document.addEventListener("keyup", (event) => {
   } else if (event.key === "ArrowDown") {
     socket.send(`{"Move":"Down"}`);
   } else if (event.code === "Space") {
-    if (fire.classList.contains("addFire")) return;
-    // socket.send(`{"Move":"Fire"}`);
-    fire.classList.add("addFire");
-    fire.style.backgroundImage =
-      "url(../img/fire.gif?random=" + Math.random() + ")";
-    setTimeout(() => {
-      fire.style.backgroundImage = "";
-      fire.classList.remove("addFire");
-    }, 800);
+    socket.send(`{"Move":"Fire"}`);
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (moveArray.includes(event.key)) {
+    socket.send(`{"Move":"Stop"}`);
   }
 });
 
@@ -42,6 +40,9 @@ socket.onmessage = (event) => {
   dataParsed.Players.forEach((player) => {
     if (!document.querySelector(`.tank${player.Id}`)) {
       const newTank = document.createElement("div");
+      const fire = document.createElement("div");
+      fire.classList.add(`fire${player.Id}`);
+      newTank.prepend(fire);
       newTank.classList.add(`tank${player.Id}`);
       newTank.style.position = "absolute";
       newTank.style.top = `${player.X}px`;
@@ -59,6 +60,28 @@ socket.onmessage = (event) => {
       tank.style.transform = `rotate(${player.R}deg)`;
       tank.style.left = `${player.X}px`;
       tank.style.top = `${player.Y}px`;
+      if (player.Fired === true) {
+        const fire = document.querySelector(`.fire${player.Id}`);
+        fire.style.display = "block";
+        fire.style.position = "absolute";
+        fire.style.top = "-60px";
+        fire.style.left = "30px";
+        fire.style.width = "40px";
+        fire.style.height = "80px";
+        fire.style.backgroundRepeat = "no-repeat";
+        fire.style.backgroundSize = "cover";
+        fire.style.transform = "rotate(90deg)";
+        fire.style.zIndex = "10";
+        fire.classList.add(`bullet${player.Id}`);
+        if (fire.classList.contains(`addFire${player.Id}`)) return;
+        fire.classList.add(`addFire${player.Id}`);
+        fire.style.backgroundImage =
+          "url(../img/fire.gif?random=" + Math.random() + ")";
+        setTimeout(() => {
+          fire.style.backgroundImage = "";
+          fire.classList.remove(`addFire${player.Id}`);
+        }, 800);
+      }
     }
   });
 };
